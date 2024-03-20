95a85f7b1c682f1cc7ad9689be5611db6328cd45
Suman <parbatkhanal73@gmail.com>
2024-03-20 09:33:30 +0545

    Player Movement
    Moving player left and right

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
-    
+    public Animator jumpani;
+    public float speed = 5f;
+    public float leftright = 4f;
     void Start()
     {
-        
+        jumpani = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>().AddForce(Vector3.back, ForceMode.VelocityChange);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.World);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * leftright, Space.World);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * leftright, Space.World);
+        }
+
+        /*   if (Input.GetKeyDown(KeyCode.Space))
+           {
+               jumpani.SetBool("jump", true);
+           }*/
+
     }
 }
