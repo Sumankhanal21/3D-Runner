1b776c271970852b93c6f2d1cc37bae1114ac13f
Suman <parbatkhanal73@gmail.com>
2024-04-02 19:37:29 +0545

    3d runner
    coin bug fixing

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class CoinScript : MonoBehaviour
 {
-   // private int coins = 0;
-    //public int rotate = 1;
-    public AudioSource touchsound;
-   // void Update()
-   // {
-     //   transform.Rotate(0, rotate, 0, Space.World);
-   // }
+   private int coins = 0;
+    public int rotate = 2;
+    //public AudioSource touchsound;
+    void Update()
+    {
+        transform.Rotate(0, rotate, 0, Space.World);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.transform.tag == "coin") { 
-            touchsound.Play();
-        this.gameObject.SetActive(false);
-           // coins++;
-           // Debug.Log(coins);
+        if (other.tag == "Player") {
+          //  touchsound.Play();
+             //this.gameObject.SetActive(false);
+             coins++;
+             Debug.Log(coins);
+            Destroy(gameObject);
+          //  Destroy(other.gameObject);
 
-      //  }
+        }
     }
 }
