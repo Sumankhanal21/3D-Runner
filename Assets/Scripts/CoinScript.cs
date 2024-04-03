d90ebea9f6e35922639160c14b22cdf5ec9a2b40
Suman <parbatkhanal73@gmail.com>
2024-04-03 19:05:33 +0545

    3D runner

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -6,7 +6,12 @@ public class CoinScript : MonoBehaviour
 {
    private int coins = 0;
     public int rotate = 2;
-    //public AudioSource touchsound;
+    public AudioSource touchsound;
+
+    private void Start()
+    {
+        touchsound = GetComponent<AudioSource>();
+    }
     void Update()
     {
         transform.Rotate(0, rotate, 0, Space.World);
@@ -14,12 +19,12 @@ public class CoinScript : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player") {
-          //  touchsound.Play();
-             //this.gameObject.SetActive(false);
+          touchsound.Play();
+             
              coins++;
              Debug.Log(coins);
             Destroy(gameObject);
-          //  Destroy(other.gameObject);
+         
 
         }
     }
